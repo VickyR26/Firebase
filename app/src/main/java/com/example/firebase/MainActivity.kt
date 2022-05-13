9b63c9dd6a6b72e0cda2c7acc3a5003fa787ba5e
vicky <rake.vrrm@gmail.com>
2022-05-12 21:03:23 -0500

    commit con Usuarios

--- app/src/main/java/com/example/firebase/MainActivity.kt
+++ app/src/main/java/com/example/firebase/MainActivity.kt
@@ -27,10 +27,28 @@ class MainActivity : AppCompatActivity() {
         //myRef.child("ejemplo").child("4").setValue (Card("4", "negro", "picas"))
         //myRef.child("ejemplo").child("5").setValue (Card("5", "negro", "treboles"))
 
+        /*
         myRef.child("ejemplo").get().addOnSuccessListener { response ->
             Log.d("firebaseResponse", response.value.toString())
         }.addOnFailureListener{
             Log.e("firebaseResponse", "Error getting data", it)
         }
+        */
+
+        /*
+        myRef.child("Usuarios").child("Usuario 1").setValue (Users("Carla", "carlita@gmail.com", "Amo4Miperro2000", 30))
+        myRef.child("Usuarios").child("Usuario 2").setValue (Users("José", "Josesito@gmail.com", "MonarcAs1010", 18))
+        myRef.child("Usuarios").child("Usuario 3").setValue (Users("Marianita", "MarMar@gmail.com", "BTSESlomejorDElMunDo00", 16))
+        myRef.child("Usuarios").child("Usuario 4").setValue (Users("Ana", "Anita22@gmail.com", "Micumpleaños", 22))
+        myRef.child("Usuarios").child("Usuario 5").setValue (Users("Luis", "luis_crack@gmail.com", "Roblox", 14))
+        */
+
+
+        myRef.child("Usuarios").get().addOnSuccessListener { response ->
+            Log.d("firebaseResponse", response.value.toString())
+        }.addOnFailureListener{
+            Log.e("firebaseResponse", "Error getting data", it)
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/firebase/Users.kt
@@ -0,0 +1,9 @@
+package com.example.firebase
+
+class Users (nombre: String, email:String, contraseña: String, edad:Int) {
+    val nombre = nombre
+    val email = email
+    val contraseña = contraseña
+    val edad = edad
+
+}
